33d9cbd33fce0692d146ac15ed6d4a8be5d4939d
Senna <test@hotmail.com>
2020-04-16 23:03:51 +0200

    Create UI tour test

---
+++ MyModelWorkout/MyModelWorkoutUITests/MyModelWorkoutUITests.swift
@@ -0,0 +1,27 @@
+//
+//  MyModelWorkoutUITests.swift
+//  MyModelWorkoutUITests
+//
+//  Created by Sanae Zine on 16/04/2020.
+//  Copyright © 2020 Senna Zine. All rights reserved.
+//
+
+import XCTest
+
+class TourTest: XCTestCase {
+
+    func testExample() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+         XCTAssert(app.staticTexts["Hallo welkom bij de MyModelWorkout App"].exists)
+        app.buttons["Next"].tap()
+        XCTAssert(app.staticTexts["workoutInfo"].exists)
+        app.buttons["Next2"].tap()
+         XCTAssert(app.staticTexts["Start je gezonde lifestyle vandaag nog!"].exists)
+        app.buttons["Next3"].tap()
+        
+        XCTAssert(app.staticTexts["Aanmelden"].exists)
+        
+    }
+}
